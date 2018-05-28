52d4ee47d5537b181c8f4f19acbd7e7611aabd46
Christopher Carson <chris@nowzoo.com>
2018-05-28 09:29:26 -0500

    removed old version of the lib

--- projects/modal/src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of modal
- */
-
-export * from './lib/modal.service';
-export * from './lib/modal.component';
-export * from './lib/interfaces';
-export * from './lib/modal.module';
